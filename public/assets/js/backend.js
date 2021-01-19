054f85e423fe86a5165c362a0989096c4a6a9bab
Bunks <busk_1@yahoo.com>
2021-01-19 17:57:46 -0500

    psuedo coded all the routes out

--- public/assets/js/backend.js
+++ public/assets/js/backend.js
@@ -1,30 +1,56 @@
 // Dependencies
 // =============================================================
-var express = require("express");
-var path = require("path");
+const express = require("express");
+const path = require("path");
+import { v4 as uuidv4 } from "uuid";
 
 // Sets up the Express App
 // =============================================================
-var app = express();
-var PORT = 3000;
+const app = express();
+const PORT = 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 // app.use(express.static(path.join(__dirname, './public')));
 
-// Routes
+// GET Routes
 // =============================================================
 
-// Home index route.
-app.get("/index", function (req, res) {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-// Webpage after get notes button is hit.
+// Return the notes.html page.
 app.get("/notes", function (req, res) {
   res.sendFile(path.join(__dirname, "notes.html"));
 });
+
+// Webpage after get notes button is hit.
+app.get("/api/notes", function (req, res) {
+  // TODO get the notes contained in the notes.db
+  // Read the notes into a variable and return the variable.
+});
+
+// Home route.
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+// POST Routes
+// =============================================================
+
+app.post("/api/notes", function (req, res) {
+  // TODO get the notes contained in the notes.db
+  // -- add the current note being sent to the "database" to the other notes
+  // -- write all the notes with fs.writefile with a unique id.
+});
+
+// DELETE Routes
+// =============================================================
+
+app.delete("/api/notes/:id", function (req, res) {
+  // Get the current notes in notes.db.
+  // Loop over them and delete the one equal to id being provided.
+  // Write notes back into db.
+});
+
 
 // Starts the server to begin listening
 // =============================================================
