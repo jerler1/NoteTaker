f67be445649e3a839f1e3ab9d006cfba9ef5d136
Bunks <busk_1@yahoo.com>
2021-01-19 20:32:39 -0500

    added process.env.PORT

--- backend.js
+++ backend.js
@@ -9,7 +9,7 @@ const { stringify } = require("querystring");
 // Sets up the Express App
 // =============================================================
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
